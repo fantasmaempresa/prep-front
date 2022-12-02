bf0907f8f77344515e843e754a853eaf35fa40c5
Luis Angel Osorio <luis.angel.osorio@outlook.com>
2022-12-01 19:31:01 -0600

    :sparkles: Modulo datos estadisticos

--- src/app/features/statistics/statistics.module.ts
+++ src/app/features/statistics/statistics.module.ts
@@ -3,16 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { StatisticsRoutingModule } from './statistics-routing.module';
 import { StatisticsComponent } from './page/statistics/statistics.component';
-import { SharedModule } from "../../shared/shared.module";
-
+import { SharedModule } from '../../shared/shared.module';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
 
 @NgModule({
-  declarations: [
-    StatisticsComponent
-  ],
-  imports: [
-    SharedModule,
-    StatisticsRoutingModule
-  ]
+  declarations: [StatisticsComponent],
+  imports: [SharedModule, NgxChartsModule, StatisticsRoutingModule],
 })
-export class StatisticsModule { }
+export class StatisticsModule {}
