bfe480726b61f2f37c385b4e6ea4d5cff24cf1c4
Luis Angel Osorio <luis.angel.osorio@outlook.com>
2023-01-05 17:54:35 -0600

    :closed_lock_with_key: Actualizand creds prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   base_url: 'http://159.223.133.133:8080/api',
-  client_id: '97d5252b-6d1c-4606-bf24-a141d8336b11',
-  client_secret: 'SvHnyq15Ue1vog9mjDAbaEwlL7R0EdBzwXG8XdZf',
+  client_id: '97ef5224-d53b-4de1-88cb-88e52bd2ff51',
+  client_secret: 'ucQ2NmTdv4vq2yxZlZZ2X4T7DiFsHjsUM1i087JL',
   grant_type: 'password',
   mix_pusher_app_key: '1234567',
   mix_pusher_app_cluster: 'mt1',
