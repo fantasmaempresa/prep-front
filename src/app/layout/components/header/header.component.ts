d5f6bb23e1354ffe340baab4e069a324a20a4ab4
raitoforce <mahgt_34@hotmail.com>
2023-02-09 14:53:44 -0600

    🩹 fix logout action

--- src/app/layout/components/header/header.component.ts
+++ src/app/layout/components/header/header.component.ts
@@ -1,5 +1,8 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { MatSlideToggleChange } from '@angular/material/slide-toggle';
+import { Store } from '@ngrx/store';
+import { logout } from '../../../state/auth/auth.actions';
+import { MessageHelper } from '../../../shared/helpers/MessageHelper';
 
 @Component({
   selector: 'app-header',
@@ -10,7 +13,19 @@ export class HeaderComponent {
   @Output()
   readonly darkModeSwitched = new EventEmitter<boolean>();
 
+  constructor(private store: Store) {}
+
   changeTheme({ checked }: MatSlideToggleChange) {
     this.darkModeSwitched.emit(checked);
+  }
+
+  logout() {
+    MessageHelper.decisionMessage(
+      'Cerrar Sesión',
+      '¿Estas seguro de cerrar la sesión?',
+      () => {
+        this.store.dispatch(logout());
+      }
+    );
   }
 }
